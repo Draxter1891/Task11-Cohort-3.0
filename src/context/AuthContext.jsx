28f454f061934c4ad80338cae93a477cfe15c5c7
Rishabh Tripathi <trishabh2001@gmail.com>
2026-07-19 20:03:30 +0530

    bug fixed: reset() usage in AUTH context

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
 import { nanoid } from "nanoid";
 import { Children, createContext, useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, } from "react-hook-form";
 import { useNavigate } from "react-router";
 
 export const Auth = createContext();
@@ -10,7 +10,7 @@ export const AuthContext = ({ children }) => {
   const [users, setUsers] = useState(() => {
     return JSON.parse(localStorage.getItem("users")) || [];
   });
-
+ const {reset} = useForm();
   const [currentUser, setCurrentUser] = useState(() => {
     return JSON.parse(localStorage.getItem("currentUser")) || null;
   });
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,12 @@ import Navbar from '../components/Navbar'
 import { Auth } from '../context/AuthContext'
 
 const Home = () => {
-  const {user,setUser} = useContext(Auth);
+  
   return (
     <div>
       <h1 className='font-[clash] text-2xl text-white'>this is font 1, 12341234563490</h1>
       <h1 className='font-[font2] text-2xl text-white'>this is font 2, 13242345789987</h1>
       <h1 className='font-[font3] text-2xl text-white'>this is font 3, 1345293457234970</h1>
-      <h2 className='font-[clash] text-2xl text-white'>{user}</h2>
-      <button onClick={()=>setUser("Rishabh") } className='text-white bg-blue-500'>change user</button>
     </div>
   )
 }
